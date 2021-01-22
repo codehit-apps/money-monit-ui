2e3001100fad87c343ff8064a01562baa64c4637
Marvin <marvin@codehit.net>
2021-01-22 09:57:23 +0800

    add payment indicator

--- src/components/DebtCard.vue
+++ src/components/DebtCard.vue
@@ -26,19 +26,23 @@
           <div class="label">{{ dateLabel }}</div>
           <div class="value">{{formatDate(debt.debited_at)}}</div>
         </div>
+        <div class="record-line">
+          <div class="label">Pending Payments</div>
+          <div class="value"><ion-badge v-if="pendingPaymentCount > 0" color="danger">{{ pendingPaymentCount }}</ion-badge></div>
+        </div>
       </div>
     </ion-card-content>
   </ion-card>
 </template>
 
 <script>
-import { findWhere } from 'underscore';
+import { findWhere, each } from 'underscore';
 import { pesoFormatter, dateFormatter } from '../helper'
-import { IonCard, IonCardContent } from '@ionic/vue';
+import { IonCard, IonCardContent, IonBadge } from '@ionic/vue';
 
 export default {
   name: 'DebtCard',
-  components: {IonCard, IonCardContent},
+  components: {IonCard, IonCardContent, IonBadge},
   props: {
     debt: Object,
     mode: String,
@@ -52,6 +56,13 @@ export default {
     },
     isRecievable: function () {
       return this.mode == "recievable"
+    },
+    pendingPaymentCount: function () {
+      let count = 0
+      each(this.debt.debt_payments, function (item) {
+        if (item.approved_at == null) count += 1
+      })
+      return count
     }
   },
   methods: {
