a2e0c359860b69bbd4223d2023d225a911324800
Marvin Tangpos <marvin@codehit.net>
2023-12-26 09:49:45 +0800

    Add service worker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router';
+import './registerServiceWorker'
+import router from './router'
 
 import { IonicVue } from '@ionic/vue';
 
@@ -31,7 +32,7 @@ const app = createApp(App)
   .use(IonicVue)
   .use(router)
   .use(store);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
